6fdab893427f7fdda59a952b46f3c49c5d637f4a
Ted Sluis <ted.sluis@gmail.com>
2018-10-05 20:00:04 +0200

    final version

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -1,4 +1,4 @@
-sudo docker tag vote-reset-front:latest tedsluis/vote-front:v1
+sudo docker tag vote-front:latest tedsluis/vote-front:v1
 sudo docker tag vote-reset-front:latest tedsluis/vote-reset-front:v1
 sudo docker tag vote-multi-front:latest tedsluis/vote-multi-front:v1
 sudo docker push tedsluis/vote-front:v1
